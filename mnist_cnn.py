1183a29e4dd0a0811754d09573f71f3b51d4b1a0
Evensgn <i@evensgn.com>
2017-07-20 17:13:54 +0800

    Update cnn code

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -54,14 +54,14 @@ CONV_LAYERS = len(CONV_L) - 1
 DENSE_L = [7 * 7 * 64, 1024, 10]
 DENSE_LAYERS = len(DENSE_L) - 1
 
-learning_rate = 2e-5
+learning_rate = 5e-4
 iterations = 20000
-batch_size = 100
-regular_lambda = 1e-4
-drop_keep_prob = 1.0
+batch_size = 50
+regular_lambda = 1e-6
+drop_keep_prob = 0.5
 
 keep_prob = tf.placeholder("float")
-x = tf.placeholder(tf.float32, [None, num_pixel])
+x = tf.placeholder("float", [None, num_pixel])
 y_ = tf.placeholder("float", [None, 10])
 
 conv_W = list(range(CONV_LAYERS + 1))
@@ -71,10 +71,11 @@ for i in range(CONV_LAYERS):
     conv_b[i + 1] = bias_variable([CONV_L[i + 1]])
 
 conv_yt = list(range(CONV_LAYERS + 1))
-conv_yt[0] = tf.reshape(x, [-1, 28, 28, CONV_L[0]])
+conv_yt_pool = list(range(CONV_LAYERS + 1))
+conv_yt_pool[0] = tf.reshape(x, [-1, 28, 28, CONV_L[0]])
 for i in range(CONV_LAYERS):
-    conv_yt[i + 1] = tf.nn.relu(conv2d(conv_yt[i], conv_W[i + 1]) + conv_b[i + 1])
-    conv_yt[i + 1] = max_pool_2x2(conv_yt[i + 1])
+    conv_yt[i + 1] = tf.nn.relu(conv2d(conv_yt_pool[i], conv_W[i + 1]) + conv_b[i + 1])
+    conv_yt_pool[i + 1] = max_pool_2x2(conv_yt[i + 1])
 
 dense_W = list(range(DENSE_LAYERS + 1))
 dense_b = list(range(DENSE_LAYERS + 1))
@@ -83,59 +84,15 @@ for i in range(DENSE_LAYERS):
     dense_b[i + 1] = bias_variable([DENSE_L[i + 1]])
 
 dense_yt = list(range(DENSE_LAYERS + 1))
-dense_yt[0] = tf.reshape(conv_yt[CONV_LAYERS], [-1, DENSE_L[0]])
+dense_yt[0] = tf.reshape(conv_yt_pool[CONV_LAYERS], [-1, DENSE_L[0]])
 for i in range(DENSE_LAYERS):
     if i == DENSE_LAYERS - 1:
         dense_yt[i] = tf.nn.dropout(dense_yt[i], keep_prob)
-    dense_yt[i + 1] = tf.nn.relu(tf.matmul(dense_yt[i], dense_W[i + 1]) + dense_b[i + 1])
+        dense_yt[i + 1] = tf.matmul(dense_yt[i], dense_W[i + 1]) + dense_b[i + 1]
+    else:
+        dense_yt[i + 1] = tf.nn.relu(tf.matmul(dense_yt[i], dense_W[i + 1]) + dense_b[i + 1])
 
 y = tf.nn.softmax(dense_yt[DENSE_LAYERS])
-
-'''
-# Create the model
-# placeholder
-x = tf.placeholder("float", [None, 784])
-y_ = tf.placeholder("float", [None, 10])
-# variables
-W = tf.Variable(tf.zeros([784,10]))
-b = tf.Variable(tf.zeros([10]))
-
-y = tf.nn.softmax(tf.matmul(x,W) + b)
-
-# first convolutinal layer
-w_conv1 = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
-
-x_image = tf.reshape(x, [-1, 28, 28, 1])
-
-h_conv1 = tf.nn.relu(conv2d(x_image, w_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
-
-# second convolutional layer
-w_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
-
-h_conv2 = tf.nn.relu(conv2d(h_pool1, w_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
-
-# densely connected layer
-w_fc1 = weight_variable([7*7*64, 1024])
-b_fc1 = bias_variable([1024])
-
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, w_fc1) + b_fc1)
-
-# dropout
-keep_prob = tf.placeholder("float")
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
-# readout layer
-w_fc2 = weight_variable([1024, 10])
-b_fc2 = bias_variable([10])
-
-y = tf.nn.softmax(tf.matmul(h_fc1_drop, w_fc2) + b_fc2)
-
-'''
 
 l2_loss = 0
 for i in range(CONV_LAYERS):
